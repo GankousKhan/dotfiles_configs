2ee0385d7efe55dbc7b79b77f9fd64f9d10abeae
Christopher Lamb <christopher.lamb@protonmail.com>
2021-01-14 11:25:31 -0500

    Windows personal

---
+++ Windows/post-install.ps1
@@ -0,0 +1,51 @@
+# Developer Tools
+winget install VSCodium.VSCodium
+winget install Microsoft.VisualStudio.Community
+winget intsall Insomnia.Insomnia
+winget install Insomnia.InsomniaDesigner
+winget install OpenJS.NodeJS
+winget install LINQPad.LINQPad6
+winget install SourceFoundry.HackFonts
+winget install GitHub.GitHubDesktopBeta
+winget install GitHub.cli
+winget install Telerik.Fiddler
+winget install Docker.DockerDesktop
+winget install dnGrep.dnGrep
+winget install Microsoft.dotnet
+winget install JetBrains.Toolbox
+
+# Modeling and CAD
+winget install OpenSCAD.OpenSCAD
+winget install KiCad.KiCad
+
+# Windows
+winget install Microsoft.PowerToys
+winget install Microsoft.PowerShell-Preview
+winget install Microsoft.WindowsTerminalPreview
+
+# Security + Privacy
+winget install ProtonTechnologies.ProtonVPN
+winget install Bitwarden.Bitwarden
+winget install AxCrypt.AxCrypt
+
+# Internet
+winget install Mozilla.FirefoxDeveloperEdition
+winget install DelugeTeam.Deluge
+winget install zerotier.zerotier
+
+# Video + Music
+winget install VideoLAN.VLC
+
+# Misc
+winget install 7zip.7zip
+
+# Gaming
+winget install Valve.Steam
+winget install EpicGames.EpicGamesLauncher
+
+# Chat
+winget install Discord.Discord
+winget install Signal.Signal
+winget install Microsoft.Teams
+winget install Telegram.TelegramDesktop
+winget install Zoom.Zoom
